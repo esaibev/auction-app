07679d30193a23f91d1a10878c2b71f49b93da42
esaibev <esaias.bevegard@gmail.com>
2023-10-23 15:22:02 +0200

    Remove property ‘Id’ in BidVM

--- lab2/ViewModels/BidVM.cs
+++ lab2/ViewModels/BidVM.cs
@@ -4,7 +4,6 @@ namespace lab2.ViewModels
 {
 	public class BidVM
 	{
-        public int Id { get; set; }
         public string Bidder { get; set; }
         public int Amount { get; set; }
         public DateTime DateMade { get; set; }
@@ -13,7 +12,6 @@ namespace lab2.ViewModels
         {
             return new BidVM()
             {
-                Id = bid.Id,
                 Bidder = bid.Bidder,
                 Amount = bid.Amount,
                 DateMade = bid.DateMade
